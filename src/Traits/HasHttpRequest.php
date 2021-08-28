2046b319d0844846254c26d507c8a7c30cc381cd
zhangxiang <gobeyond@petalmail.com>
2021-08-28 19:03:37 +0800

    perf: 设置中间件

--- src/Traits/HasHttpRequest.php
+++ src/Traits/HasHttpRequest.php
@@ -325,6 +325,29 @@ trait HasHttpRequest
     }
 
     /**
+     * 添加一个中间件
+     *
+     * @param callable $middleware
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function pushMiddleware(callable $middleware, $name = null)
+    {
+        if(!is_null($name) && ($this->app->config['middleware'][$name]['enable'] ?? '') === false) {
+            return $this;
+        }
+
+        if (is_null($name)) {
+            array_push($this->middleware, $middleware);
+        } else {
+            $this->middleware[$name] = $middleware;
+        }
+
+        return $this;
+    }
+
+    /**
      * 获取所有的中间件
      *
      * @return array
